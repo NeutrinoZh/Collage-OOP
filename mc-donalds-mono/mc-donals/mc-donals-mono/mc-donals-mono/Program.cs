cf876a46942f473b09b4eea8c7cfd801e5f8a03a
NeutrinoZh <neutrinozh@gmail.com>
2022-09-11 22:48:23 +0300

    mc-donalds

---
+++ mc-donalds-mono/mc-donals/mc-donals-mono/mc-donals-mono/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace mcdonalsmono {
+    class Program {
+        private static void Successively() {
+            Console.WriteLine("Successively:");
+
+            Thread.Sleep(3000);
+            Console.WriteLine("Finish Task 1");
+
+            Thread.Sleep(2000);
+            Console.WriteLine("Finish Task 2");
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Finish Task 3");
+        }
+
+        private static void Wait() {
+            Console.WriteLine("Wait:");
+
+            Task[] tasks = new Task[3] {
+                () => {
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Finish Task 1");
+                },
+                () => {
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Finish Task 2");
+                },
+                () => {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Finish Task 3");
+                }
+            }
+        
+
+            foreach (var item in tasks)
+                    item.Start();
+        }
+
+        public static async Task Main(string[] args) {
+            Successively();
+            Wait();
+        }
+    }
+}
