6025297b0950233292b137d1532deeba72dbb70a
NeutrinoZh <neutrinozh@gmail.com>
2022-09-14 12:25:44 +0300

    http

--- mc-donalds-mono/mc-donals/mc-donals-mono/mc-donals-mono/Program.cs
+++ mc-donalds-mono/mc-donals/mc-donals-mono/mc-donals-mono/Program.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using System.Threading;
 
-namespace mcdonalsmono {
+namespace mcdonals {
     class Program {
         private static void Successively() {
             Console.WriteLine("Successively:");
@@ -21,28 +21,79 @@ namespace mcdonalsmono {
             Console.WriteLine("Wait:");
 
             Task[] tasks = new Task[3] {
-                () => {
+                new Task(() => {
                     Thread.Sleep(3000);
                     Console.WriteLine("Finish Task 1");
-                },
-                () => {
+                }),
+                new Task(() => {
                     Thread.Sleep(2000);
                     Console.WriteLine("Finish Task 2");
-                },
-                () => {
+                }),
+                new Task(() => {
                     Thread.Sleep(1000);
                     Console.WriteLine("Finish Task 3");
-                }
-            }
-        
+                })
+            };
 
             foreach (var item in tasks)
-                    item.Start();
+                item.Start();
+
+            Task.WaitAll(tasks);
         }
 
-        public static async Task Main(string[] args) {
+        public static void First() {
+            Console.WriteLine("First:");
+
+            Task[] tasks = new Task[3] {
+                new Task(() => {
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Finish Task 1");
+                }),
+                new Task(() => {
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Finish Task 2");
+                }),
+                new Task(() => {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Finish Task 3");
+                })
+            };
+
+            foreach (var item in tasks)
+                item.Start();
+
+            Task.WaitAny(tasks);
+        }
+
+        public static void All() {
+            Console.WriteLine("All:");
+
+            Task[] tasks = new Task[3] {
+                new Task(() => {
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Finish Task 1");
+                }),
+                new Task(() => {
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Finish Task 2");
+                }),
+                new Task(() => {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Finish Task 3");
+                })
+            };
+
+            foreach (var item in tasks)
+                item.Start();
+
+            Thread.Sleep(5000);
+        }
+
+        public static void Main(string[] args) {
             Successively();
             Wait();
+            All();
+            First();
         }
     }
 }
